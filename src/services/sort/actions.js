4786102a50548b66e9cabc4a737c9ef3a4d6759a
Puja Jagani <puja.jagani93@gmail.com>
2024-09-09 17:42:07 +0530

    Do not sort when sorting is from highest to lowest price

--- src/services/sort/actions.js
+++ src/services/sort/actions.js
@@ -1,6 +1,11 @@
 import { UPDATE_SORT } from './actionTypes';
 
-export const updateSort = sort => ({
-  type: UPDATE_SORT,
-  payload: sort
-});
+export const updateSort = sort => {
+  if (sort === 'highestprice') {
+  sort = '';
+  }
+  return {
+    type: UPDATE_SORT,
+    payload: sort
+  };
+};
