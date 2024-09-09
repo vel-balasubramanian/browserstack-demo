933e06298b3bfe5c3c186c4a49c844e2c9bc1514
Puja Jagani <puja.jagani93@gmail.com>
2024-09-09 18:08:11 +0530

    "Products Found" displayed in bigger font for Safari

--- src/components/Shelf/ShelfHeader/index.jsx
+++ src/components/Shelf/ShelfHeader/index.jsx
@@ -4,13 +4,21 @@ import PropTypes from 'prop-types';
 import Sort from '../Sort';
 import Clearfix from '../../Clearfix';
 
+import {
+  isSafari,
+} from "react-device-detect";
+
+
 const ShelfHeader = props => {
   return (
     <div className="shelf-container-header" style={{marginRight: '50px'}}>
       <small className="products-found">
+      {isSafari ? (
+        <h1 style={{ fontSize: '24px' }}> <span>{props.productsLength} Product(s) found.</span> </h1> 
+      ) : (
         <span>{props.productsLength} Product(s) found.</span>
+      )}
       </small>
-      <Sort />
       <Clearfix />
     </div>
   );
