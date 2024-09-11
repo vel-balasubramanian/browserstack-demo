e7e206cda56c34cbd06dc93598b971d3ef833956
Puja Jagani <puja.jagani93@gmail.com>
2024-09-11 14:56:52 +0530

    Add Bird Eats Bug widget

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -18,7 +18,60 @@ class App extends Component {
 
     // Bug: failed request
     axios.get('/failed-request').catch((e) => console.log(e));
+
+    // Initialize Bird Eats Bug widget
+    (function() {
+      const birdeatsbug = (window.birdeatsbug = window.birdeatsbug || []);
+      if (birdeatsbug.initialize) return;
+      if (birdeatsbug.invoked) {
+        if (window.console && console.error) {
+          console.error('birdeatsbug snippet included twice.');
+        }
+        return;
+      }
+      birdeatsbug.invoked = true;
+      birdeatsbug.methods = [
+        'setOptions',
+        'trigger',
+        'resumeSession',
+        'takeScreenshot',
+        'startRecording',
+        'stopRecording',
+        'stopSession',
+        'uploadSession',
+        'deleteSession'
+      ];
+      birdeatsbug.factory = function(method) {
+        return function() {
+          const args = Array.prototype.slice.call(arguments);
+          args.unshift(method);
+          birdeatsbug.push(args);
+          return birdeatsbug;
+        };
+      };
+      for (let i = 0; i < birdeatsbug.methods.length; i++) {
+        const key = birdeatsbug.methods[i];
+        birdeatsbug[key] = birdeatsbug.factory(key);
+      }
+      birdeatsbug.load = function() {
+        const script = document.createElement('script');
+        script.type = 'module';
+        script.async = true;
+        script.src = 'https://sdk.birdeatsbug.com/v2/core.js';
+        const mountJsBefore = document.getElementsByTagName('script')[0] || document.body.firstChild;
+        mountJsBefore.parentNode.insertBefore(script, mountJsBefore);
+        const style = document.createElement('link');
+        style.rel = 'stylesheet';
+        style.type = 'text/css';
+        style.href = 'https://sdk.birdeatsbug.com/v2/style.css';
+        const mountCssBefore = document.querySelector('link[rel="stylesheet"]') || mountJsBefore;
+        mountCssBefore.parentNode.insertBefore(style, mountCssBefore);
+      };
+      birdeatsbug.load();
+      window.birdeatsbug.setOptions({ publicAppId: process.env.BIRD_EATS_BUG_PUBLIC_APP_ID });
+    })();
   }
+
   render() {
     const { is2G } = this.state;
     if(is2G === null) {
